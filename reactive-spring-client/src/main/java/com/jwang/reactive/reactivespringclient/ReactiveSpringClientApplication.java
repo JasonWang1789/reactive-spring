49b963c6bbc4cf4249f398d794cdd3a509ace5e8
Jason Wang <jasonwang1789@gmail.com>
2020-03-29 14:00:37 -0400

    initial commit of reactive-spring-client

---
+++ reactive-spring-client/src/main/java/com/jwang/reactive/reactivespringclient/ReactiveSpringClientApplication.java
@@ -0,0 +1,13 @@
+package com.jwang.reactive.reactivespringclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveSpringClientApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ReactiveSpringClientApplication.class, args);
+  }
+
+}
