348176f1879911f908d8e45157bc35aff38f4f46
Jason Wang <jasonwang1789@gmail.com>
2020-04-18 20:00:46 -0400

    added module-info.java

---
+++ reactive-spring-client/src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.jwang.reactive.reactivespringclient {
+  requires org.reactivestreams;
+  requires reactor.core;
+  requires spring.boot;
+  requires spring.boot.autoconfigure;
+  requires spring.context;
+  requires spring.data.commons;
+  requires spring.data.mongodb;
+  requires spring.webflux;
+  requires static lombok;
+}
